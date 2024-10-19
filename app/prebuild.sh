bd6c7c1b356a6495004d214b858ed21ff9c06398
elementdavv <elementdavv@hotmail.com>
2024-10-19 08:54:12 +0800

    add prebuild

---
+++ app/prebuild.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -x
+
+rm -rf ../jni/libmupdf/thirdparty/lcms2/testbed/
+rm -rf ../jni/libmupdf/thirdparty/lcms2/Projects/VC2013/lcms2mt.sln
+rm -rf ../jni/libmupdf/thirdparty/lcms2/utils/delphi/
+rm -rf ../jni/libmupdf/thirdparty/harfbuzz/test/fuzzing
+rm -rf ../jni/libmupdf/thirdparty/harfbuzz/test/api/fonts/clusterfuzz-testcase-minimized-hb-shape-fuzzer-5753845452636160
+rm -rf ../jni/libmupdf/thirdparty/extract/test
+find ../ -name build.gradle -exec sed -i -E '/^\s{12}maven\s*\{\s*$/,/^\s{12}\}\s*$/d' {} \;
+find ../ -name build.gradle -exec sed -i -E '/maven\s*\{/d' {} \;
